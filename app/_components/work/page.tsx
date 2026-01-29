997086ad892da095ef4f01018df125fe4c05766b
kanny <w24001@osfl.ac.jp>
2026-01-29 12:30:33 +0900

    fix

--- app/_components/work/page.tsx
+++ app/_components/work/page.tsx
@@ -43,7 +43,7 @@ export default function WorkPage() {
             <div className={styles.work}>
               <h3 className={styles.workTitle}>Smart Budget</h3>
               <p>チームで作った家計簿アプリ（卒業制作）</p>
-              <Link href="hhttps://github.com/Kanny-git/school-project" target="_blank" className={styles.link}>GitHub リポジトリはこちら →</Link>
+              <Link href="https://github.com/Kanny-git/school-project" target="_blank" className={styles.link}>GitHub リポジトリはこちら →</Link>
               <br />
                <Link href="https://smtbudget.vercel.app" target="_blank">
                 <div className={styles.demoLink}>View Demo ⇀</div>
